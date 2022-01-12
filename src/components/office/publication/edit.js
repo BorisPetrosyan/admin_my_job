f9cf01dc220418d154cef0da8df9cba4845327f9
BorisPetrosyan <borispetrosyans8@gmail.com>
2022-01-12 18:36:07 +0400

    formik make

--- src/components/office/publication/edit.js
+++ src/components/office/publication/edit.js
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
-
+import { useFormik } from 'formik';
 import { connect } from "react-redux";
 import { toast } from "react-toastify";
-
+import {number, object, string} from 'yup';
 import { t_create_glossary, t_save_glossary } from "../../../redux/tracks";
 
 import CONSTANTS from "./constants";
+import {generateModelForRequest} from "../../../utils/util";
 
 const PublicationsEdit = (props) => {
   const { history } = props;
-  console.log(props.location)
+  // console.log(props.location)
   const { _id } = props.location.state.publication
     ? props.location.state.publication
     : null;
@@ -31,7 +32,48 @@ const PublicationsEdit = (props) => {
   const [publication, setPublication] = useState(
     state.publication.docs ? newPublication : state.publication
   );
-  console.log(publication)
+  const validationSchema = object().shape({
+    name_en: string().required('Please enter a Name'),
+    name_ru: string().required('Please enter a Name'),
+    author_en:string().required('Please enter a Name'),
+    author_ru: string().required('Please enter a Name'),
+    subject_en:string().required('Please enter a Name'),
+    subject_ru: string().required('Please enter a Name'),
+  });
+
+  const {
+		resetForm,
+		handleChange,
+		handleSubmit,
+		values,
+		errors,
+		validateForm,
+		setErrors,
+		setValues,
+		setFieldValue,
+		touched
+	} = useFormik({
+		initialValues: newPublication,
+		validateOnChange: true,
+		validateOnBlur: true,
+		onSubmit: ({  name_en,
+                     name_ru,
+                     author_en,
+                     author_ru,
+                     subject_en,
+                     subject_ru,
+                     isActive, }) => createPublication(name_en,
+            name_ru,
+            author_en,
+            author_ru,
+            subject_en,
+            subject_ru,
+            isActive,
+		),
+		validationSchema,
+		validateOnMount: false,
+	});
+
   const lang = state?.lang || 'ru';
   const changeInput = (field, value) => {
     console.log(field, value)
@@ -50,10 +92,12 @@ const PublicationsEdit = (props) => {
     // saveGlossary(glossar);
   };
   const createPublication = (e) => {
-    e.preventDefault();
-    history.push("/glossary");
-    const { createGlossary } = props;
-    createGlossary(publication);
+    // let err = false;
+    // e.preventDefault();
+    alert('done')
+    // history.push("/glossary");
+    // const { createGlossary } = props;
+    // createGlossary(publication);
   };
   // const handleImageUpload = (acceptedFiles) => {
   //   const {
@@ -73,7 +117,7 @@ const PublicationsEdit = (props) => {
 
 
   const saveEvent = state.publication.docs ? createPublication : savePublication;
-   console.log(saveEvent)
+    console.log(errors)
   return (
     <div className="content content-profile">
       <div className="container-fluid">
@@ -82,42 +126,50 @@ const PublicationsEdit = (props) => {
             <div className="form-photo ">
             </div>
             <div className="sub-title">{`${CONSTANTS[lang].header_edit}`}</div>
-            <form className="form row " id="form-add">
+            <form className="form row " id="form-add" onSubmit={handleSubmit}>
               <div className="col-4">
               <div className="input-group">
                 <div className="label">{`${CONSTANTS[lang].name} ${CONSTANTS[lang].inEn}`}</div>
                 <input
+                    id='name_en'
+                    name='name_en'
                   type="text"
                   className="input-text"
-                  value={publication.name_en}
-                  onChange={(e) => changeInput("name_en", e.target.value)}
+                  value={values.name_en}
+                  onChange={handleChange}
                 />
               </div>
               <div className="input-group">
                 <div className="label">{`${CONSTANTS[lang].name} ${CONSTANTS[lang].inRu}`}</div>
                 <input
+                  id='name_ru'
+                  name='name_ru'
                   type="text"
                   className="input-text"
-                  value={publication.name_ru}
-                  onChange={(e) => changeInput("name_ru", e.target.value)}
+                  value={values.name_ru}
+                  onChange={handleChange}
                 />
               </div>
               <div className="input-group">
                 <div className="label">{`${CONSTANTS[lang].author} ${CONSTANTS[lang].inEn}`}</div>
                 <input
+                    id='author_en'
+                    name='author_en'
                     type="text"
                   className="input-text"
-                  value={publication.author_en}
-                  onChange={(e) => changeInput("author_en", e.target.value)}
+                  value={values.author_en}
+                  onChange={handleChange}
                 />
               </div>
               <div className="input-group">
                 <div className="label">{`${CONSTANTS[lang].author} ${CONSTANTS[lang].inRu}`}</div>
                 <input
+                    id='author_ru'
+                    name='author_ru'
                     type="text"
                   className="input-text"
-                  value={publication.author_ru}
-                  onChange={(e) => changeInput("author_ru", e.target.value)}
+                  value={values.author_ru}
+                  onChange={handleChange}
                 />
               </div>
               </div>
@@ -125,19 +177,23 @@ const PublicationsEdit = (props) => {
               <div className="input-group">
                 <div className="label">{`${CONSTANTS[lang].subject} ${CONSTANTS[lang].inEn}`}</div>
                 <textarea
+                    id='subject_en'
+                    name='subject_en'
                     type="textarea"
                   className="input-text"
-                  value={publication.subject_en}
-                  onChange={(e) => changeInput("section_en", e.target.value)}
+                  value={values.subject_en}
+                  onChange={handleChange}
                 />
               </div>
               <div className="input-group">
                 <div className="label">{`${CONSTANTS[lang].subject} ${CONSTANTS[lang].inRu}`}</div>
                 <textarea
+                    id='subject_ru'
+                    name='subject_ru'
                     type="textarea"
                   className="input-text"
-                  value={publication.subject_ru}
-                  onChange={(e) => changeInput("section_ru", e.target.value)}
+                  value={values.subject_ru}
+                  onChange={handleChange}
                 />
               </div>
 
@@ -163,23 +219,25 @@ const PublicationsEdit = (props) => {
                 {/*    onChange={(e) => changeInput("link", e.target.value)}*/}
                 {/*/>*/}
               </div>
+              <div className="btn-confirm">
+                <a onClick={(e) => cancel(e)} href="#!" className="btn-confirm__cancel">
+                  {CONSTANTS[lang].cancel}
+                </a>
+                <button
+                    type='submit'
+                    // onClick={(e) => saveEvent(e)}
+                    // href="#!"
+                    className="btn-confirm__ok"
+                    data-form="#form-add"
+                >
+                  {CONSTANTS[lang].save}
+                </button>
+              </div>
             </form>
           </div>
         </div>
       </div>
-      <div className="btn-confirm">
-        <a onClick={(e) => cancel(e)} href="#!" className="btn-confirm__cancel">
-          {CONSTANTS[lang].cancel}
-        </a>
-        <a
-          onClick={(e) => saveEvent(e)}
-          href="#!"
-          className="btn-confirm__ok"
-          data-form="#form-add"
-        >
-          {CONSTANTS[lang].save}
-        </a>
-      </div>
+
     </div>
   );
 };
